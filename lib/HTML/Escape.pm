b118be6703b39373340af7a0fb3928340bcff80b
tokuhirom <tokuhirom@gmail.com>
2012-09-30 15:09:55 +0900

    initial import

---
+++ lib/HTML/Escape.pm
@@ -0,0 +1,39 @@
+package HTML::Escape;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+HTML::Escape - A module for you
+
+=head1 SYNOPSIS
+
+  use HTML::Escape;
+
+=head1 DESCRIPTION
+
+HTML::Escape is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
